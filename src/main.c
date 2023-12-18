46e7142545c620eb280b315dd40808e61c7cc93c
Colin Matherne <colin.matherne1@gmail.com>
2023-12-18 16:15:55 -0500

    last commit doesn't work, fixed in this one. When I fixed the memory leak I didn't realize I as assigning argv to cmd_cpy

--- src/main.c
+++ src/main.c
@@ -5,24 +5,26 @@
 #include <string.h>
 #include <sys/wait.h>
 
-void divideString(char *cmd, char *delim, int *argc, char **argv);
+#define MAX_ARGS 255
+
+void divideString(char *cmd, char *delim, char **argv);
 
 int main()
 {
 	while (1) {
 		char *cmd = NULL; char *delim = " \n"; size_t n = 0;
-		int argc; char **argv = NULL; pid_t pid = getpid();
+		int i; char **argv = NULL; pid_t pid = getpid();
 
 		printf("> ");
 		if (getline(&cmd, &n, stdin) == -1)
 			return -1;
 
-		divideString(cmd, delim, &argc, NULL);				/* need JUST argc to malloc argv */
-		if ((argv = (char **)malloc(sizeof(char *) * argc)) == NULL) {
-			perror("Could not allocate memoty");
+		if ((argv = (char **)malloc(sizeof(char *) * MAX_ARGS)) == NULL) {
+			perror("Could not allocate memory");
 			exit(EXIT_FAILURE);
 		}
-		divideString(cmd, delim, NULL, argv);
+		divideString(cmd, delim, argv);
+		free(cmd);
 
 		pid = fork();
 		if (pid == 0)
@@ -32,32 +34,24 @@ int main()
 			}
 		wait(&pid);
 
-		free(cmd);
+		for (i = 0; argv[i] != NULL; i++)
+			free(argv[i]);
 		free(argv);
 	}
 
 	return 0;
 }
 
-void divideString(char *cmd, char *delim, int *argc, char **argv) {
-	char *token;
+void divideString(char *cmd, char *delim, char **argv) {
+	char *token	= NULL;												/* One Ring */
 	int8_t i = 0;
 	char *cmd_cpy = strdup(cmd);
 
-	token = strtok(cmd_cpy, delim);									/* One Ring */
-		if (argv != NULL) {
-		for (i = 0; token; i++) {
-			argv[i] = token;
-			token = strtok(NULL, delim);							/* To Rule  */
-		}
-		argv[i] = NULL;
-	} else {
-		for (i = 0; token; i++) {
-			token = strtok(NULL, delim);							/* Them All */
-		}
+	token = strtok(cmd_cpy, delim);									/* To Rule  */
+	for (i = 0; token; i++) {
+		argv[i] = strdup(token);
+		token = strtok(NULL, delim);								/* Them All */
 	}
-
-	if (argc != NULL)
-		*argc = i;
+	argv[i] = NULL;
 	free(cmd_cpy);
 }
